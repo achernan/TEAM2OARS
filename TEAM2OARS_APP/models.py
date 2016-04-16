fe878afc8fe0c2131e72def7703621f4ea1070d5
Gaby Cruz <gcruz.a10@gmail.com>
2016-04-15 22:33:52 -0500

    added Testimonies (3 total rows) of data into DB using models.py and shell

--- TEAM2OARS_APP/models.py
+++ TEAM2OARS_APP/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Testimonies(models.Model):
+    testimonial_id = models.IntegerField(primary_key=True)
+    testimonial_date = models.DateField()
+    testimonial_content = models.TextField()
+    tenant_ss = models.IntegerField(null=False)
