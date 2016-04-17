b695fb5ab9e67918dfd48b57f799e0f38c464173
Gaby Cruz <gcruz.a10@gmail.com>
2016-04-16 21:25:16 -0500

    displaying DB content in testimonials.html now need search functionality

--- TEAM2OARS_APP/views.py
+++ TEAM2OARS_APP/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Testimonies
+from django.template import loader
+from django.http import HttpResponse
 
 def front_page(request):
     return render(request, 'TEAM2OARS_APP/front_page.html', {})
@@ -10,4 +13,9 @@ def contact_us(request):
     return render(request, 'TEAM2OARS_APP/contact_us.html', {'contact': contact_us})
 
 def testimonials(request):
-    return render(request, 'TEAM2OARS_APP/testimonials.html', {'testimonials': testimonials})
+    allTestimonies = Testimonies.objects.all()
+    template = loader.get_template('TEAM2OARS_APP/testimonials.html')
+    context = {
+        'allTestimonies': allTestimonies
+    }
+    return HttpResponse(template.render(context, request))
