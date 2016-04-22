0affb4d4a2785c59ff9f04bb77704e75ba4c1dac
Gaby Cruz <gcruz.a10@gmail.com>
2016-04-22 18:57:23 -0500

    working login. acceptable username and password combos (all lowercase) will be in Tenant and Staff tables (can view in admin console)

--- TEAM2OARS_APP/urls.py
+++ TEAM2OARS_APP/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.front_page, name='front_page'),
+    url(r'^enter_credentials/$', views.enter_credentials, name='enter_credentials'),
     url(r'^about/$', views.about_us, name='about_us'),
     url(r'^login/$', views.login, name='tenant_login'),
     url(r'^contact/$', views.contact_us, name='contact_us'),
--- TEAM2OARS_APP/views.py
+++ TEAM2OARS_APP/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from .models import Testimonies
+from .models import Staff
+from .models import Tenant
 from django.template import loader
 from django.http import HttpResponse
 
 def front_page(request):
     return render(request, 'TEAM2OARS_APP/front_page.html', {})
+
+def enter_credentials(request):
+    context = False
+
+    if Tenant.objects.filter(username__exact=request.POST['uname']):
+        if Tenant.objects.filter(password__exact=request.POST['pwd']):
+            urecords = Tenant.objects.filter(username__exact=request.POST['uname'])
+            precords = Tenant.objects.filter(password__exact=request.POST['pwd'])
+            context = {
+                'urecords': urecords, 'query': urecords,
+                'precords': precords, 'query': precords
+            }
+
+    if Staff.objects.filter(username__exact=request.POST['uname']):
+        if Staff.objects.filter(password__exact=request.POST['pwd']):
+            urecords = Staff.objects.filter(username__exact=request.POST['uname'])
+            precords = Staff.objects.filter(password__exact=request.POST['pwd'])
+            context = {
+                'urecords': urecords, 'query': urecords,
+                'precords': precords, 'query': precords
+            }
+
+    if context:
+        return render(request, 'TEAM2OARS_APP/tenant_login.html', context)
+    else:
+        return render(request, 'TEAM2OARS_APP/front_page.html', {})
+
+
 
 def about_us(request):
     return render(request, 'TEAM2OARS_APP/about_us.html', {'about': about_us})
