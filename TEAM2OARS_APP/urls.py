8b8d9832ed66fa947c2d58a74f097b48420082d3
idamo <emmanuel.obiahu@gmail.com>
2016-04-24 21:44:59 -0500

    Don't know why, but it works idk idk

--- TEAM2OARS_APP/urls.py
+++ TEAM2OARS_APP/urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
     url(r'^$', views.front_page, name='front_page'),
     url(r'^enter_credentials/$', views.enter_credentials, name='enter_credentials'),
     url(r'^about/$', views.about_us, name='about_us'),
-    url(r'^login/$', views.login, name='tenant_login'),
+    url(r'^tenantWelcome/$', views.login, name='tenant_login'),
+    url(r'^assistantLogin/$', views.assistantLogin, name='assistant_login'),
+    url(r'^managerLogin/$', views.managerLogin, name='manager_login'),
+    url(r'^supervisorLogin/$', views.supervisorLogin, name='supervisor_login'),
     url(r'^contact/$', views.contact_us, name='contact_us'),
     url(r'^testimonials/$', views.testimonials, name='testimonials'),
+    #url(r'^tenantslist/$', views.tenantslist, name='tenantslist'),
     url(r'^search/$', views.search, name='search')
 ]
--- TEAM2OARS_APP/views.py
+++ TEAM2OARS_APP/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Testimonies
 from .models import Staff
 from .models import Tenant
+from .models import Invoices
+from .models import Apartment
+from .models import Automobiles
 from django.template import loader
 from django.http import HttpResponse
 
@@ -15,23 +18,43 @@ def enter_credentials(request):
         if Tenant.objects.filter(password__exact=request.POST['pwd']):
             urecords = Tenant.objects.filter(username__exact=request.POST['uname'])
             precords = Tenant.objects.filter(password__exact=request.POST['pwd'])
+            allTenants = Tenant.objects.all()
+            for info in urecords:
+              invoices = Invoices.objects.filter(tenant_ss__exact=info.tenant_ss)
+              apartment = Apartment.objects.filter(tenant_ss__exact=info.tenant_ss)
+              automobiles = Automobiles.objects.filter(tenant_ss__exact=info.tenant_ss)
             context = {
                 'urecords': urecords, 'query': urecords,
-                'precords': precords, 'query': precords
+                'precords': precords, 'query': precords,
+                'invoices': invoices, 'apartment' : apartment,
+                'automobiles': automobiles, 'allTenants' : allTenants
             }
+        if context:
+          return render(request, 'TEAM2OARS_APP/tenant_login.html', context)
+        else:
+          return render(request, 'TEAM2OARS_APP/front_page.html', {})
 
     if Staff.objects.filter(username__exact=request.POST['uname']):
         if Staff.objects.filter(password__exact=request.POST['pwd']):
             urecords = Staff.objects.filter(username__exact=request.POST['uname'])
             precords = Staff.objects.filter(password__exact=request.POST['pwd'])
+            allTenants = Tenant.objects.all()
+            allApartments = Apartment.objects.all()
             context = {
                 'urecords': urecords, 'query': urecords,
-                'precords': precords, 'query': precords
+                'precords': precords, 'query': precords,
+                'allTenants' : allTenants, 'allApartments' : allApartments
             }
-
-    if context:
-        return render(request, 'TEAM2OARS_APP/tenant_login.html', context)
-    else:
+            if urecords.filter(position__contains='supervisor'):
+              if context:
+                return render(request, 'TEAM2OARS_APP/supervisor_login.html', context)
+            if urecords.filter(position__contains="manager"):
+              if context:
+                return render(request, 'TEAM2OARS_APP/manager_login.html', context)
+            if urecords.filter(position__contains="assistant"):
+              if context:
+                return render(request, 'TEAM2OARS_APP/assistant_login.html', context)
+              
         return render(request, 'TEAM2OARS_APP/front_page.html', {})
 
 
@@ -48,6 +71,9 @@ def login(request):
 def testimonials(request):
     return render(request, 'TEAM2OARS_APP/testimonials.html', {'testimonials': testimonials})
 
+#def assistantLogin(request):
+ #   return render(request, 'TEAM2OARS_APP/assistant_login.html', {'assistantLogin': assistantLogin})
+
 def search(request):
     allTestimonies = Testimonies.objects.filter(testimonial_content__contains=request.GET['q'])
     template = loader.get_template('TEAM2OARS_APP/search.html')
@@ -59,3 +85,35 @@ def search(request):
     elif Testimonies.objects.count < 1:
         return HttpResponse(template.render({'allTestimonies': allTestimonies}, request))
 
+def assistantLogin(request):
+    allTenants = Tenant.objects.all()
+    template = loader.get_template('TEAM2OARS_APP/assistant_login.html')
+    if Tenant.objects.count > 1:
+        context = {
+            'allTenants': allTenants
+        }
+        return HttpResponse(template.render(context, request))
+    elif Tenant.objects.count < 1:
+        return HttpResponse(template.render({'allTenants': allTenants}, request))
+
+def managerLogin(request):
+    allTenants = Tenant.objects.all()
+    template = loader.get_template('TEAM2OARS_APP/manager_login.html')
+    if Tenant.objects.count > 1:
+        context = {
+            'allTenants': allTenants
+        }
+        return HttpResponse(template.render(context, request))
+    elif Tenant.objects.count < 1:
+        return HttpResponse(template.render({'allTenants': allTenants}, request))
+        
+def supervisorLogin(request):
+    allTenants = Tenant.objects.all()
+    template = loader.get_template('TEAM2OARS_APP/supervisor_login.html')
+    if Tenant.objects.count > 1:
+        context = {
+            'allTenants': allTenants
+        }
+        return HttpResponse(template.render(context, request))
+    elif Tenant.objects.count < 1:
+        return HttpResponse(template.render({'allTenants': allTenants}, request))
